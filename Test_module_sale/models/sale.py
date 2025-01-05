9be0dbd164feb9d4e560ec7921c8a4609f98f62b
Siripong-Sangso <siripong.sangso@gmail.com>
2025-01-05 22:54:21 +0700

    finish report no credit note

--- Test_module_sale/models/sale.py
+++ Test_module_sale/models/sale.py
@@ -1,6 +1,7 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
+
 class ResCompanyInherit(models.Model):
     _inherit = 'res.company'
 
@@ -50,6 +51,38 @@ class SaleOrderInherit(models.Model):
     _sql_constraints = [
         ('unique_project_number', 'unique(project_number)', 'The Project Number must be unique!'),
     ]
+    amount_total_text_th = fields.Char(
+        string="Amount Total Text (TH)",
+        compute="_compute_amount_total_text_th",
+        store=True
+    )
+
+    @api.depends('amount_total')
+    def _compute_amount_total_text_th(self):
+        for order in self:
+            order.amount_total_text_th = self._number_to_thai_text(order.amount_total)
+
+    def _number_to_thai_text(self, number):
+        thai_num = ["", "หนึ่ง", "สอง", "สาม", "สี่", "ห้า", "หก", "เจ็ด", "แปด", "เก้า"]
+        thai_unit = ["", "สิบ", "ร้อย", "พัน", "หมื่น", "แสน", "ล้าน"]
+        result = ""
+        number_str = str(int(number))
+        number_len = len(number_str)
+
+        for i in range(number_len):
+            digit = int(number_str[i])
+            unit_index = number_len - i - 1
+
+            if digit != 0:
+                if unit_index == 1 and digit == 1:
+                    result += "สิบ"
+                elif unit_index == 1 and digit == 2:
+                    result += "ยี่สิบ"
+                elif unit_index == 0 and digit == 1 and number_len > 1:
+                    result += "เอ็ด"
+                else:
+                    result += thai_num[digit] + thai_unit[unit_index]
+        return result
 
     @api.constrains('partner_mobile')
     def _check_partner_mobile(self):
@@ -64,6 +97,9 @@ class SaleOrderInherit(models.Model):
                 raise ValidationError("The Project Number must be at least 5 characters long.")
 
 
+class SaleOrderLineInherit(models.Model):
+    _inherit = 'sale.order.line'
 
-
-
+    @api.model
+    def get_line_count(self):
+        return len(self.name.splitlines())
