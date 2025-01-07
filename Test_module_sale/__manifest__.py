8513053690ac1052b2ba819c90c9767753dbc723
Siripong-Sangso <siripong.sangso@gmail.com>
2025-01-07 16:25:53 +0700

    edit __manifest__
    delete company.xml in __manifest__

--- Test_module_sale/__manifest__.py
+++ Test_module_sale/__manifest__.py
@@ -10,7 +10,6 @@
     'depends': ['base', 'sale'],
     'data': [
         'security/ir.model.access.csv',
-        'views/company.xml',
         'views/sale.xml',
         'reports/report.xml',
         'reports/sale_report_template.xml',
