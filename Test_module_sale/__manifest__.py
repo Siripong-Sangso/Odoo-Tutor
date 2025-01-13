a51aec12b054a4951ba345dcac8b7694bfc3586f
Siripong-Sangso <siripong.sangso@gmail.com>
2025-01-13 10:29:56 +0700

    add invoice_report __manifest__

--- Test_module_sale/__manifest__.py
+++ Test_module_sale/__manifest__.py
@@ -18,6 +18,7 @@
         'reports/quotation_report_infp.xml',
         'reports/credit_notes_report_infp.xml',
         'reports/credit_notes_report_infp_H.xml',
+        'reports/invoice_report_infp_H.xml',
         'reports/Test3.xml',
     ],
     'demo': [
